8971d23c7679aba4064f3c6b9717177010999b7b
SunoopDogg <aswoo55555@gmail.com>
2026-01-16 17:21:45 +0000

    refactor: Update BIM object and prediction entity types for batch processing

--- src/5entities/bim-object/model/types.ts
+++ src/5entities/bim-object/model/types.ts
@@ -1,8 +1,9 @@
 export interface BIMObjectInput {
-  ifc_type: string;
+  object_type: string;
   category: string;
   family_name: string;
   family: string;
   type: string;
   type_id: string;
+  pps_code: string;
 }
